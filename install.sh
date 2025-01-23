4a68825fec354687ee2641858c56b790c6a572cc
João André <joaocandre@gmail.com>
2025-01-23 01:12:23 +0000

    Add .desktop shortcut

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #! /bin/bash
 
+# install scripts on local path
 INSTALL_DIR="/usr/local/bin"
-
 cp set_next_boot.sh ${INSTALL_DIR}
 cp reboot_into_windows.sh ${INSTALL_DIR}
+
+# install windows icon
+# cf. https://martin.hoppenheit.info/blog/2016/where-to-put-application-icons-on-linux/
+INSTALL_DIR="/usr/share/icons/hicolor/scalable/apps/"
+cp resources/windows-logo-dark.svg ${INSTALL_DIR}
+
+# install desktop shortcut
+INSTALL_DIR="/usr/share/applications"
+cp reboot_into_windows.desktop ${INSTALL_DIR}
